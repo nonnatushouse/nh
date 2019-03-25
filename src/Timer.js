444afae2c93806e53b819bd222782527701bf087
NH <nonnatushouse@gmail.com>
2019-03-25 16:35:42 +0100

    Timer-adjustment to put days in focus

--- src/Timer.js
+++ src/Timer.js
@@ -5,11 +5,13 @@ import useInterval from "./useInterval";
 
 export default function Timer(props) {
   const [date, setDate] = useState(new Date());
-  const targetDate = new Date(Date.UTC(2019, 11, 25, 20, 0, 0));
+  const targetDate = getTargetDate();
 
   useInterval(() => {
     setDate(new Date());
   }, 1000);
+
+  const note = new Date(Date.UTC(2019, 11, 25, 20, 0, 0)) - date > 0 ?  "Note: This is assuming that the episode airs on December 25th, 2019 at 20:00 UTC" : "";
 
   const timeLeft = targetDate - date;
   return (
@@ -19,8 +21,13 @@ export default function Timer(props) {
         Time until Call the Midwife Series 9 airs:
       </h1>
       <TimerDisplay timeLeft={timeLeft} />
+      <div className="timer-note">{note}</div>
       </div>
     </>
     
   );
+}
+
+function getTargetDate() {
+  return new Date(Date.UTC(2019, 11, 25, 20, 0, 0));
 }
--- src/TimerDisplay.js
+++ src/TimerDisplay.js
@@ -2,30 +2,51 @@ import React from "react";
 import "./Timer.css";
 
 export default function TimerDisplay({ timeLeft }) {
+  const seconds = Math.floor((timeLeft / 1000) % 60);
+  const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+  const hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
 
-    const seconds = Math.floor((timeLeft/ 1000) % 60);
-    const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
-    const hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
-    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  let secondString = "Seconds";
+  let minuteString = "Minutes";
+  let hourString = "Hours";
+  let dayString = "Days";
+
+  if (days === 1) {
+    dayString = "Day";
+  }
+
+  if (hours === 1) {
+    hourString = "Hour";
+  }
+
+  if (minutes === 1) {
+    minuteString = "Minute";
+  }
+  if (seconds === 1) {
+    secondString = "Second";
+  }
 
   return (
-    <div className="timer-slots-container">
-      <div className="timer-slot-container">
-        <div className="timer-slot">{days}</div>
-        <div className="timer-slot-text">Days</div>
+    <>
+      <div className="day-slot-container">
+        <div className="day-slot">{days}</div>
+        <div className="day-slot-text">{dayString}</div>
       </div>
-      <div className="timer-slot-container">
-        <div className="timer-slot">{hours}</div>
-        <div className="timer-slot-text">Hours</div>
+      <div className="timer-slots-container">
+        <div className="timer-slot-container">
+          <div className="timer-slot">{hours}</div>
+          <div className="timer-slot-text">{hourString}</div>
+        </div>
+        <div className="timer-slot-container">
+          <div className="timer-slot">{minutes}</div>
+          <div className="timer-slot-text">{minuteString}</div>
+        </div>
+        <div className="timer-slot-container">
+          <div className="timer-slot">{seconds}</div>
+          <div className="timer-slot-text">{secondString}</div>
+        </div>
       </div>
-      <div className="timer-slot-container">
-        <div className="timer-slot">{minutes}</div>
-        <div className="timer-slot-text">Minutes</div>
-      </div>
-      <div className="timer-slot-container">
-        <div className="timer-slot">{seconds}</div>
-        <div className="timer-slot-text">Seconds</div>
-      </div>
-    </div>
+    </>
   );
 }
