9822992add9663594cfd068f236ddf24a7b457a9
NH <nonnatushouse@gmail.com>
2019-10-06 10:37:46 +0200

    Added title to subs

--- src/Subs.js
+++ src/Subs.js
@@ -7,6 +7,7 @@ const initialState = {
   result: [],
   subs: [],
   seasons: new Map(),
+  titles: new Map(),
   selectedSeason: 1,
   selectedEpisode: 1
 };
@@ -21,6 +22,7 @@ export default function Subs() {
         ...state,
         result,
         seasons: getSeasonsAndEpisodes(result),
+        titles: getTitles(result),
         subs: result.filter(
           chunk =>
             chunk.season === state.selectedSeason &&
@@ -47,6 +49,15 @@ export default function Subs() {
     });
   }
 
+  function getEpisodeTitle() {
+    const key = String(state.selectedSeason) + String(state.selectedEpisode);
+    let episodeTitle = "";
+    if (state.titles.has(key)) {
+      episodeTitle = state.titles.get(key);
+    }
+    return episodeTitle;
+  }
+
   return (
     <div className="subs-container">
       <EpisodeSelector
@@ -55,6 +66,8 @@ export default function Subs() {
         seasons={state.seasons}
         onClick={updateSeasonAndEpisode}
       ></EpisodeSelector>
+
+      <div className="episode-title">{getEpisodeTitle()}</div>
 
       {state.subs.map(chunk => (
         <>
@@ -89,7 +102,7 @@ function getSeasonsAndEpisodes(result) {
   const seasons = new Map();
 
   for (const chunk of result) {
-    const { season, episode } = chunk;
+    const { season, episode, display_name } = chunk;
 
     if (!seasons.has(season)) {
       seasons.set(season, new Set());
@@ -97,4 +110,17 @@ function getSeasonsAndEpisodes(result) {
     seasons.get(season).add(episode);
   }
   return seasons;
+}
+
+function getTitles(result) {
+  const titles = new Map();
+
+  for (const chunk of result) {
+    const { season, episode, display_name } = chunk;
+    const key = String(season) + String(episode);
+    if (!titles.has(key)) {
+      titles.set(key, display_name);
+    }
+  }
+  return titles;
 }
