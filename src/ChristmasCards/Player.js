760f0488d9865b50182c49ea88cac55ca73b19d2
NH <nonnatushouse@gmail.com>
2020-12-15 13:02:54 +0100

    toggle audio after 3 sec of page loading

--- src/ChristmasCards/Player.js
+++ src/ChristmasCards/Player.js
@@ -4,7 +4,7 @@ const useAudio = url => {
   
   const [audio] = useState(new Audio(url));
   audio.load()
-  const [playing, setPlaying] = useState(true);
+  const [playing, setPlaying] = useState(false);
 
   const toggle = () => setPlaying(!playing);
 
@@ -20,6 +20,16 @@ const useAudio = url => {
       audio.removeEventListener('ended', () => setPlaying(false));
     };
   }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log('This will run after 3 seconds!')
+      toggle()
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+
 
   return [playing, toggle];
 };
