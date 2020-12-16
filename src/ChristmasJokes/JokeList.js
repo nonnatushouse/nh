ef9d38a6df7c1568a0dd24982ffb6b49e7a84f76
NH <nonnatushouse@gmail.com>
2020-12-16 13:53:48 +0100

    Made jokes even jokier

--- src/ChristmasJokes/JokeList.js
+++ src/ChristmasJokes/JokeList.js
@@ -5,11 +5,11 @@ import "./Joke.css";
 export default function JokeList() {
 
     const jokes = [
-        {question: "What does Sister Monica Joan have in her head?", punchline: "Nun-sense!"},
+        {question: "What is Sister Monica Joan's head full of?", punchline: "Nun-sense!"},
         {question: "How come Sister Julienne saw The Sound of Music a second time?", punchline: "She got into a habit"},
-        {question: "Which is the most up-lifting character?", punchline: "Phyllis CRANE"},
+        {question: "Which is the most up-lifting CtM character?", punchline: "Phyllis CRANE"},
         {question: "Why did Mother Jesu-Emanuel forbid the Sister from wearing perfume at the Motherhouse?", punchline: "She would not tolerate such nun-scents"},
-        {question: "How many Nonnatuns does it take to change a lightbulb?", punchline: "Nun!"},
+        {question: "How many Nonnatuns does it take to change a lightbulb?", punchline: "Nun! (Fred does it)"},
         {question: "How come Sister Bernadette quit being a nun?", punchline: "She kicked the habit"},
         {question: "Who handles the biggest Nonnatus deliveries?", punchline: "Nurse Crane"},
         {question: "What did Sister Julienne say when Trixie asked for an opinion on her new hairstyle?", punchline: "\"Franklin my dear, I don't give a damn\""},
